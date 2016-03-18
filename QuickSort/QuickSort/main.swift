66f1328d86a1f11d562570cc683f9f4560276e14
Phuc .Ng <phucnguyen.2411@gmail.com>
2016-03-18 16:42:59 +0700

    QuickSort
    @011

--- QuickSort/QuickSort/main.swift
+++ QuickSort/QuickSort/main.swift
@@ -30,6 +30,39 @@ func findPivot(i:Int, j:Int, A:[Int]) -> Int{
 }
 
 
-func findPartition(i:Int, j:Int, pivot:Int, A:[Int]) -> [Int]{
+func findPartition(i:Int, j:Int, pivot:Int, var A:[Int]) -> Int{
+    var L:Int = i
+    var R:Int = j
+    while R < L {
+        while A[L] < pivot{
+            L++
+        }
+        while A[R] >= pivot{
+            R--
+        }
+        if ( L < R ){
+            swap(&A[L], &A[R])
+        }
     
+    }
+    let partition = L
+    return partition
 }
+
+func quickSort(i:Int, j:Int, A:[Int]){
+    let pivot = findPivot(i, j: j, A: A)
+    if pivot == -1{
+        return
+    }
+    let partition:Int = findPartition(i, j: j, pivot: pivot, A: A)
+    quickSort(i, j: partition - 1, A: A)
+    quickSort(partition, j: j, A: A)
+}
+
+
+
+//test
+
+var mangA:[Int] = [ 1,3,5,7,5,3,532,2,35,3,2,6]
+quickSort(0, j: mangA.count - 1, A: mangA)
+
